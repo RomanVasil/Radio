ff451af45076e9d164fb325bef443e37df58e74f
RomanVasil <jostik85@outlook.com>
2022-05-06 15:10:54 +0400

    version 1

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,4 +1,45 @@
 package ru.netology;
 
 public class Radio {
+    private int station;
+    private int maxVolume = 10;
+    private int minVolume = 0;
+    private int volume;
+    private boolean on;
+
+    public int getStation() {
+        return station;
+    }
+    public int setStation(int station){
+        if (station == 9) {
+             this.station = 0;
+        }
+        else {
+            this.station = station + 1;
+        }
+        return station;
+    }
+
+    public int getVolume(){
+        return volume;
+    }
+    public int setPlusVolume(int plusVolume){
+        if (plusVolume < maxVolume){
+            this.volume = plusVolume + 1;
+        }
+        if(plusVolume == maxVolume){
+            this.volume = maxVolume;
+        }
+        return volume;
+    }
+    public int setMinusVolume(int minusVolume){
+        if (minusVolume < maxVolume && minusVolume > minVolume){
+            this.volume = minusVolume - 1;
+        }
+        if(minusVolume == minVolume){
+            this.volume = minVolume;
+        }
+        return volume;
+    }
 }
+
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -1,7 +1,41 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
+    @Test
+    public void shouldUseRadioStation() {
+        Radio radioSt = new Radio();
+        radioSt.setStation(9);
+        int expected = 0;
+        int actual = radioSt.getStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldUseRadioPlusVolume() {
+        Radio radioPlusVol = new Radio();
+        radioPlusVol.setPlusVolume(9);
+        int expected = 10;
+        int actual = radioPlusVol.getVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldUseRadioMinusVolume() {
+        Radio radioMinusVol = new Radio();
+        radioMinusVol.setMinusVolume(9);
+        int expected = 8;
+        int actual = radioMinusVol.getVolume();
+
+        assertEquals(expected, actual);
+    }
 }
+
+
+
